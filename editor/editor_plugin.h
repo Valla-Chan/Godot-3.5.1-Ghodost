7472b257886e4160daaa088ac8ee6892b3b8a8dc
Allison Ghost <allisongrunder@gmail.com>
2024-02-07 23:15:55 -0800

    add open_scene to editor_plugin

--- editor/editor_plugin.h
+++ editor/editor_plugin.h
@@ -172,6 +172,8 @@ public:
 
 	//TODO: send a resource for editing to the editor node?
 
+	void load_scene(const String &p_scene, bool p_set_inherited = false);
+
 	void add_control_to_container(CustomControlContainer p_location, Control *p_control);
 	void remove_control_from_container(CustomControlContainer p_location, Control *p_control);
 	ToolButton *add_control_to_bottom_panel(Control *p_control, const String &p_title, bool p_reparent = true);
